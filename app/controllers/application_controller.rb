3da70c7ab6ab5fb9f2b117e7b3a4778a8a8dd166
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-19 13:01:56 +0300

    Fix linter error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,29 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    JWT.encode(payload, 'secret')
+  end
 
-    def encode_token(payload)
-        JWT.encode(payload, 'secret')
+  def decode_token
+    auth_header = request.headers['Authorization']
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'secret')
+      rescue JWT::DecodeError
+        nil
+      end
     end
+  end
 
-    def decode_token
-        auth_header = request.headers['Authorization']
-        if auth_header
-            token = auth_header.split(' ')[1]
-            begin
-                JWT.decode(token, 'secret')
-            rescue JWT::DecodeError
-                nil
-            end
-        end
+  def authozied_user
+    decode_token = decode_token()
+    if decode_token
+      user_id = decode_token[0]['user_id']
+      @user = User.find_by(id: user_id)
     end
+  end
 
-    def authozied_user
-        decode_token = decode_token()
-        if decode_token
-            user_id = decode_token[0]['user_id']
-            @user = User.find_by(id: user_id)
-        end
-    end
-
-    def auhorized
-        render json: {error: 'Please log in'}, status: :unauthorized unless authozied_user
-    end
+  def auhorized
+    render json: { error: 'Please log in' }, status: :unauthorized unless authozied_user
+  end
 end
