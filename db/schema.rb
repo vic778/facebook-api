6c82668d6fe33c433c6882de18fd276ff7dfd204
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-19 12:59:55 +0300

    Create user_infor table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_091619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "user_infos", force: :cascade do |t|
+    t.text "bio"
+    t.string "profile_picture"
+    t.string "cover_picture"
+    t.string "city"
+    t.string "from"
+    t.string "relationship"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_user_infos_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "user_infos", "users", column: "users_id"
 end
