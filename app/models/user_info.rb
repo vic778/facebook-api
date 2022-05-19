edee0cd649f3ffa6ba00dfa78dc27082b324e3b0
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-19 20:41:53 +0300

    Include current_user

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,3 +1,8 @@
 class UserInfo < ApplicationRecord
   belongs_to :user
+
+  validates :bio,  presence: true
+  validates :city,  presence: true
+  validates :from,  presence: true
+  validates :relationship, presence: true
 end
