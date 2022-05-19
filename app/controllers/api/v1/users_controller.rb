2f4938bc4d8842f2fd961a8fa59964921e79edb2
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-19 13:00:36 +0300

    Add factory

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            token = encode_token({user_id: @user.id})
+            render json: {success: true, message: 'User created successfully'}, status: :created
+        else
+            render json: { success: false, message: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            render json: {success: true, message: 'User updated successfully'}, status: :ok
+        else
+            render json: { success: false, message: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {success: true, message: 'User deleted successfully'}, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
---
+++ app/models/user_info.rb
@@ -0,0 +1,5 @@
+class UserInfo < ApplicationRecord
+  belongs_to :user
+
+ 
+end
