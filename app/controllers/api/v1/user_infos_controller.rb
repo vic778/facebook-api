2792e3e200f8240329a6ca446816d8837debab7c
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-19 20:45:30 +0300

    Fix linters error

--- app/controllers/api/v1/user_infos_controller.rb
+++ app/controllers/api/v1/user_infos_controller.rb
@@ -1,42 +1,42 @@
 class Api::V1::UserInfosController < ApplicationController
-    before_action :authorized
-    before_action :set_user_info, only: [:update, :destroy]
+  before_action :authorized
+  before_action :set_user_info, only: %i[update destroy]
 
-    def index
-        @user_infos = UserInfo.all
-        render json: @user_infos
+  def index
+    @user_infos = UserInfo.all
+    render json: @user_infos
+  end
+
+  def show
+    @user_info = UserInfo.find(params[:id])
+    render json: @user_info
+  end
+
+  def create
+    @user_info = UserInfo.create(user_info_params.merge(user: @user))
+    if @user_info.save
+      render json: { success: true, message: 'UserInfo created successfully' }, status: :created
+    else
+      render json: { success: false, message: @user_info.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def show
-        @user_info = UserInfo.find(params[:id])
-        render json: @user_info
+  def update
+    # @user_info = UserInfo.find(params[:id])
+    if @user_info.update(user_info_params)
+      render json: { success: true, message: 'UserInfo updated successfully' }, status: :ok
+    else
+      render json: { success: false, message: @user_info.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @user_info = UserInfo.create(user_info_params.merge(user: @user))
-        if @user_info.save
-            render json: { success: true, message: 'UserInfo created successfully' }, status: :created
-        else
-            render json: { success: false, message: @user_info.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def update
-        # @user_info = UserInfo.find(params[:id])
-        if @user_info.update(user_info_params)
-            render json: { success: true, message: 'UserInfo updated successfully' }, status: :ok
-        else
-            render json: { success: false, message: @user_info.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+  def set_user_info
+    @user_info = @user.userInfos.find(params[:id])
+  end
 
-    private
-
-    def set_user_info
-        @user_info = @user.userInfos.find(params[:id])
-    end
-
-    def user_info_params
-        params.permit(:bio, :city, :from, :profile_picture, :cover_picture, :relationship)
-    end
+  def user_info_params
+    params.permit(:bio, :city, :from, :profile_picture, :cover_picture, :relationship)
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
     render json: @user
