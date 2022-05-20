6e78f30b816f3f0ee7c810814d6710247fd8d4ac
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-20 11:34:11 +0300

    Config carrierwave

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,5 +1,7 @@
 class UserInfo < ApplicationRecord
   belongs_to :user
+  mount_uploader :profile_picture, ImageUploader
+  mount_uploader :cover_picture, ImageUploader
 
   validates :bio, presence: true
   validates :city, presence: true
